7a1f88ed2fa0fac6eda8da8c1ef6a6af253c796a
Delia Elena Barbuta <delia-elena.barbuta@student.tuiasi.ro>
2022-12-01 16:56:30 +0200

    Navbar-uri

--- Frontend_OceanCross/src/app/shared/shared.module.ts
+++ Frontend_OceanCross/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button'
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatTableModule, MatSortModule],
-  exports:[MatButtonModule, MatTableModule, MatSortModule]
+  imports: [MatButtonModule, MatTableModule, MatSortModule, MatToolbarModule],
+  exports:[MatButtonModule, MatTableModule, MatSortModule, MatToolbarModule]
 })
 export class SharedModule { }
