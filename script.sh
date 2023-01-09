4ea8a0ecc1191c47f41997968d85cf454d9e6682
Grabovschi Adrian-Marian <adrian-marian.grabovschi@student.tuiasi.ro>
2023-01-09 15:10:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+cd /home/ubuntu/PAW/proiect-paw-tadam
+
+
 # Check if there is a newer version of the code on GitHub
 git fetch
 if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]; then
   # Stop the two systemd services
-  systemctl stop proiectPAWBackend.service
+  sudo systemctl stop proiectPAWBackend.service
   #systemctl stop proiectPAWFrontend.service
 
   # Perform pull and get the latest code
   git pull
 
   # Start the two systemd services again
-  systemctl start proiectPAWBackend.service
+  sudo systemctl start proiectPAWBackend.service
   #systemctl start proiectPAWFrontend.service
 fi
