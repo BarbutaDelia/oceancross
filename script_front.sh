a39c833f8e31bdaf77ed1b2f7afeaf36ebf3cd7d
Grabovschi Adrian-Marian <adrian-marian.grabovschi@student.tuiasi.ro>
2023-01-12 10:47:21 +0200

    Create Frontend service

---
+++ script_front.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+start() {
+   cd /home/ubuntu/PAW/proiect-paw-tadam/Frontend_OceanCross
+   npm install
+   npm start   
+#ng serve --host 0.0.0.0 > /dev/null 2>&1 &
+}
+
+stop() {
+   pid=$(ps -ef | grep 'ng start' | grep -v grep | awk '{print $2}')
+   if [ -z "$pid" ]
+   then
+       echo "Service is not running."
+   else
+       kill $pid
+   fi
+}
+
+case "$1" in
+   start)
+       start
+       ;;
+   stop)
+       stop
+       ;;
+   restart)
+       stop
+       start
+       ;;
+   *)
+       echo "Usage: $0 {start|stop|restart}"
+esac
